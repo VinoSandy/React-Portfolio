aa98b2325de43dff4ea5783a321a1054b9b860b9
Santhosh Kumar <rvinothini@yahoo.co.in>
2024-03-05 01:29:41 +0000

    content added to the Home Page

--- my-react-portfolio/src/components/Home/Home.jsx
+++ my-react-portfolio/src/components/Home/Home.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import ProfileImage from "../../assets/profile_Img.png"
+import styles from "./Home.module.css"
 
 function Home(){
 
-    return(<>Home Page</>)
+
+    return(<section className={styles.content}>
+           <div className={styles.container}>
+           <div className={styles.homeText}>
+            <p> Hi there!, I 'm </p>
+            <h3 >Vinothini Santhosh</h3>
+            <p> A Front End Web Developer passionate about creating<br/>
+                intractive web applications </p>
+           </div>
+           <div className={styles.image}>
+            <img src={ProfileImage}></img>
+           </div>
+           </div>
+          </section>)
 }
 export default Home;
