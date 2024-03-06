cb18631d89b937b8551473f45216bb71bd6ead1b
Santhosh Kumar <rvinothini@yahoo.co.in>
2024-03-06 01:09:37 +0000

    style updated for project page

--- my-react-portfolio/src/components/Projects/Projects.jsx
+++ my-react-portfolio/src/components/Projects/Projects.jsx
@@ -7,21 +7,23 @@ import styles from  "./Projects.module.css";
 function Projects(){
 
     return ( 
-             <section className={styles.container}>
-             {projects.map((project) => (
-             <div className={styles.card} key={project.id}>
-             <div className={styles.content}>
-             <h4 className={styles.projectTitle}>{project.title}</h4>
-             <img src={project.imgUrl} alt={project.title} className={styles.projectImage} />
-             <div className={styles.projectLinks}>
-             <a href={project.appLink}>App</a><br/>
-             <a href={project.Repo}>Git Repo</a>
-             </div>
-             </div>
-             </div>
-    ))}
-  </section>
-
+        <section className={styles.container}>
+        
+         {projects.map((project) => (
+          <div className={styles.card} key={project.id}>
+          <img src={project.imgUrl} alt={project.title} className={styles.projectImage} />
+          <div className={styles.content}>
+           <h4 className={styles.projectTitle}>{project.title}</h4>
+            <div className={styles.projectLinks}>
+              <a href={project.appLink}>App</a><br/>
+              <a href={project.Repo}>Git Repo</a>
+            </div>
+          </div>
+        </div>
+      ))}
+      
+    </section>
+             
     )
 }
 
