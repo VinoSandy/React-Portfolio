0eab4f877bb047dc9d58663d3b5d5a46c5da0a50
Santhosh Kumar <rvinothini@yahoo.co.in>
2024-03-06 00:03:24 +0000

    Image url updated in JSON file

--- my-react-portfolio/src/components/Projects/Projects.jsx
+++ my-react-portfolio/src/components/Projects/Projects.jsx
@@ -1,20 +1,21 @@
 import React from "react";
-import projects from "../../data/ProjectData.json"
+import projects from "../../data/ProjectData.json";
+import styles from  "./Projects.module.css";
 
 
 
 function Projects(){
 
     return ( 
-             <section className="projects-container">
+             <section className={styles.container}>
              {projects.map((project) => (
-             <div className="card" key={project.id}>
-             <div className="card-content">
-             <h4 className="project-title">{project.title}</h4>
-             <img src={project.imgUrl} alt={project.title} className="project-image" />
-             <div className="project-links">
-             <a href={project.appLink}>App Link</a><br/>
-             <a href={project.Repo}>Repository Link</a>
+             <div className={styles.card} key={project.id}>
+             <div className={styles.content}>
+             <h4 className={styles.projectTitle}>{project.title}</h4>
+             <img src={project.imgUrl} alt={project.title} className={styles.projectImage} />
+             <div className={styles.projectLinks}>
+             <a href={project.appLink}>App</a><br/>
+             <a href={project.Repo}>Git Repo</a>
              </div>
              </div>
              </div>
