b34b6f9fb7b67eff759f58b03ceef2987b2fa2cf
Santhosh Kumar <rvinothini@yahoo.co.in>
2024-03-06 02:30:22 +0000

    contact page updated

--- my-react-portfolio/src/components/Contact/Contact.jsx
+++ my-react-portfolio/src/components/Contact/Contact.jsx
@@ -2,24 +2,36 @@ import React from "react";
 import styles from "./Contact.module.css"
 
 
+
 function Contact(){
 
     return (
     <section className={styles.container}>
         <div className={styles.content}>
-            <h3><span>Let's Connect</span></h3>
+            <h3><span className={styles.span}>Let's Connect</span></h3>
             <p>I'm excited about the possibilities that lie ahead in my journey as a front-end web developer, and 
                 I'm always eager to collaborate on new projects or discuss opportunities. 
-                Whether you're a fellow developer, a potential employer, or someone with a shared passion for web development, 
-                I'd love to connect! Feel free to reach out via [Contact Information] and 
+                Whether you're a fellow developer, a potential employer, or someone with a shared passion 
+                for web development,I'd love to connect! Feel free to reach out via [Contact Information] and 
                 let's explore how we can bring your ideas to life.</p>
         </div>
         <div className={styles.contact}>
-            <h3><span>Contact Me</span></h3>
+            <h3><span className={styles.span}>Contact Me</span></h3>
             <p>santhoshkumarvinothini@gmail.com</p>
-            <a href="" target="_blank"></a>
+        </div>
+        <div className={styles.imgIcon}>
+        <a href="mailto:santhoshkumarvinothini@gmail.com" target="_blank">
+        <img src="src/assets/email.png"></img>
+        </a>
+        <a href="https://github.com/VinoSandy" target="_blank">
+        <img src="src/assets/github.png"></img>
+        </a>
+        <a href="" target="_blank">
+        <img src="src/assets/linkedin.png"></img>
+        </a>
 
         </div>
+        
 
     </section>
     )
