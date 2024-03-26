37b9fb9daf85eb90fc5e41d47c4ac87f8553b226
Santhosh Kumar <rvinothini@yahoo.co.in>
2024-03-26 11:57:55 +0000

    Bg updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ function Footer(){
 /*Footer created to display with year */
 return (<footer className={styles.footer}>
         <p>
-        &copy; Copyright:{new Date().getFullYear()} <span className={styles.span}>Vinothini Santhosh</span>
+        &copy; {new Date().getFullYear()} <span className={styles.span}>Vinothini Santhosh</span>
         </p> 
        </footer>)
 
